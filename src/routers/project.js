e2fea7f57709ffef8bafdf6bf1117098ae620c4d
unknown <jerome.celebi@ynov.com>
2020-03-06 12:22:05 +0100

    fixing & test project routing

--- src/routers/project.js
+++ src/routers/project.js
@@ -30,7 +30,7 @@ router.get("/details/:id", auth, async (req, res) => {
   const id = req.params.id;
   const user = req.user;
   const project = await Project.find({ _id: id, "users.user": user });
-  if (project) {
+  if (project.length > 0) {
     res.status(201).send({ project });
   } else {
     res.status(400).send("No project");
@@ -41,7 +41,11 @@ router.post("/edit/:id", auth, async (req, res) => {
   // Edit existing project
   try {
     const id = req.params.id;
-    const project = await Project.updateOne({ _id: id }, req.body);
+    const user = req.user;
+    const project = await Project.updateOne(
+      { _id: id, "users.user": user },
+      req.body
+    );
     res.status(201).send({ project });
   } catch (error) {
     res.status(400).send(error);
